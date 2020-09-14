615e6485f0311a5e887e1a8c24de56124051b31e
Tommy.Cirilo <tommy.v.cirilo@gmail.com>
2020-09-14 15:25:26 -0500

    working through bonuses

--- src/main/java/OrderUp.java
+++ src/main/java/OrderUp.java
@@ -7,10 +7,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.PrintWriter;
 
-
-@WebServlet(name = "HelloWorldServlet", urlPatterns = "/name")
-public class HelloWorldServlet extends HttpServlet {
-
+@WebServlet(name = "HelloWorldServlet", urlPatterns = "/order-form")
+public class OrderUp extends HttpServlet {
 
     protected void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
         res.setContentType("text/html");
@@ -21,12 +19,12 @@ public class HelloWorldServlet extends HttpServlet {
             firstName = "John";
             lastName = "Doe";
         }
-        out.println("<h1>Hello, " + firstName + lastName + "</h1>");
-
-        int count = 1;
-
-
+        out.println("<form>\n" +
+                "<label for=\"firstName\">name of product:</label><br>\n" +
+                "  <input type=\"text\" id=\"firstName\" name=\"firstName\"><br>\n" +
+                "  <label for=\"lastName\">amount:</label><br>\n" +
+                "  <input type=\"text\" id=\"lastName\" name=\"lastName\">\n" +
+                "  <br><input type=\"submit\" value=\"Submit\">" +
+                "</form>");
     }
-
-
 }
